b108e29d5ef601ea9c1a8e5fd69a9ce2c11ff824
bakhronovKhusan <husanbakhronov@gmail.com>
2023-11-02 11:09:57 +0500

    get_all_branches

---
+++ app/Http/Controllers/BranchController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Components\Helper;
+use App\Models\Branch;
+use Illuminate\Http\Request;
+
+class BranchController extends Controller
+{
+    public function get_all_branches_with_group_count(Request $request, Branch $branch)
+    {
+        $branch = $branch->newQuery();
+
+        $subdomain = Helper::getSubdomain($request->headers->get('referer'));
+
+        if ($subdomain) {
+            $branch->whereHas('company', function ($q) use ($subdomain) {
+                $q->where('subdomain', $subdomain);
+            });
+        } else {
+            $company_id = (isset($request->company_id)) ? $request->company_id : 1;
+            $branch->where('company_id', $company_id);
+        }
+
+        $branch = $branch->whereNotIn('id', [31, 31])->get();
+
+        return $branch;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BranchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Hunter\HunterController;
@@ -22,6 +23,10 @@ Route::group(['middleware' => 'redis.token.check'], function ($router) {
         Route::group(['prefix' => 'hunter'], function ($router) {
             Route::post('activate/{group_id}/{student_id}', [HunterController::class, 'activate']);
             Route::post('de-activate/{group_id}/{student_id}', [HunterController::class, 'de_activate']);
+        });
+
+        Route::group(['prefix' => 'branch'], function ($router) {
+            Route::get('get_all_branches_with_group_count', [BranchController::class, 'get_all_branches_with_group_count']);
         });
     });
 });

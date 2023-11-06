88424eecf98d8d0652c3e9e4afaad53266826c99
bakhronovKhusan <husanbakhronov@gmail.com>
2023-11-06 06:15:48 +0500

    Add action Request!

--- resources/views/offline_hunter.blade.php
+++ resources/views/offline_hunter.blade.php
@@ -51,7 +51,7 @@
                             <th>ID</th>
                             <th>Name</th>
                             <th>Phone</th>
-                            <th>Teacher</th>
+                            <th>Group time</th>
                             <th>Check chiqarish</th>
                             <th>Action</th>
                         </tr>
@@ -61,7 +61,7 @@
                             <td>@{{ row.id }}</td>
                             <td>@{{ row.name }}</td>
                             <td>@{{ row.phone }} @{{ row.phone2 }}</td>
-                            <td>@{{ row.staff }}</td>
+                            <td>@{{ row.group_time }}</td>
                             <td><a :href="row.check_url" class="btn btn-outline-dark"> Check chiqarish </a></td>
                             <td>
                                 <button @click="deleteItem(row.id)" class="btn btn-outline-info" >Mos Guruh tanlash</button>

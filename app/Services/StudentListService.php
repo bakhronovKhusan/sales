ba3d694f36c40f39c2cf880b23046ac7a9d43fd8
bakhronovKhusan <husanbakhronov@gmail.com>
2023-11-07 16:41:33 +0500

    Nothing!

--- app/Services/StudentListService.php
+++ app/Services/StudentListService.php
@@ -62,8 +62,9 @@ class StudentListService
                                         and t_staff.id is not null
                                         '. $branch_id .'
                                     ORDER BY t_groups.created_at DESC');
+        dd($results);
         foreach ($results as $key => $result) {
-            $results[$key]['groups_json'] = $result['groups_json'];
+            $results[$key][$result->groups_json] = $result[$result->groups_json];
         }
         return $results;
     }

c40a35d55bee0ca08911aabe8d0901b3b7360880
bakhronovKhusan <husanbakhronov@gmail.com>
2023-11-08 11:50:37 +0500

    Nothing!

--- app/Modules/Offline_Hunter/Routes/api.php
+++ app/Modules/Offline_Hunter/Routes/api.php
@@ -20,6 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'redis.token.check'], function ($router) {
     Route::group(['prefix' => 'v1/offline_hunter'], function ($router) {
-        Route::post('new_student_list/{branches?}',[GroupController::class, 'getGroupsWhichHasNewStudents']);
+        Route::post('new_student_list',[GroupController::class, 'getGroupsWhichHasNewStudents']);
     });
 });
